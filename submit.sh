9e93894f5d4687ad22bd4faeb5573eb64cebda70
Jiuru Li <lixue9jiu@github.com>
2022-01-28 16:16:29 -0800

    make it run

--- submit.sh
+++ submit.sh
@@ -6,7 +6,7 @@ rm -rf submit submit.zip
 mkdir -p submit
 
 # submit team.txt
-printf "Victor Zhong,vzhong\nNoah Smith,nasmith\nSanta Claus,sclaus" > submit/team.txt
+printf "Mike He,dh63\nShaoqi Wang,shaoqi\nJiuru Li,lij93" > submit/team.txt
 
 # train model
 python src/myprogram.py train --work_dir work
